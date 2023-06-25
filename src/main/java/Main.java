d49dbdc57530e92b587234e7c782c71235e3aa1d
louisarthurw <c14210048@john.petra.ac.id>
2023-06-25 21:43:56 +0700

    new envi

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -62,7 +62,7 @@ public class Main {
                 0.125f,
                 0.125f,
                 0.125f,
-                "/models/sirkuittanpatiang.obj", "C:\\Users\\ASUS ROG\\Projects\\grafkom\\proyek-uas-grafkom\\src\\main\\resources\\textures\\texture.png"
+                "/models/sirkuitfinal.obj", "C:\\Users\\ASUS ROG\\Projects\\grafkom\\proyek-uas-grafkom\\src\\main\\resources\\textures\\texture.png"
 
         ));
         objects.get(0).scaleObject(0.25f, 0.25f, 0.25f);
